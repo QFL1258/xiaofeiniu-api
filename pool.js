6d523e84916b6480cd9416cd7ff8a505f4438c86
qfl <1781052210@qq.com>
2019-01-30 09:52:26 +0800

    api

--- pool.js
+++ pool.js
@@ -1,13 +1,13 @@
-//创建mysql连接池
+/*
+*MySQL数据库连接池
+*/
 const mysql = require('mysql');
 var pool = mysql.createPool({
-  host: '127.0.0.1',
-  user: 'root',
-  password: '',
-  database: 'xiaofeiniu',
-  connectionLimit: 10 
+  host: '127.0.0.1',      //数据库地址
+  port: 3306,             //数据库端口
+  user: 'root',           //数据库管理员
+  password: '',           //数据库管理员密码
+  database: 'xiaofeiniu', //默认连接的数据库
+  connectionLimit: 10     //连接池中连接数量
 });
-//把创建好的连接池导出
 module.exports = pool;
-
-
